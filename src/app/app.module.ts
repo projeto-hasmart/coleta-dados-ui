7aa14bcf64ccd48033d7c69716bd1b88448875e5
Ryan B Fernandes <ryan.fernandes@saude.ce.gov.br>
2020-03-05 19:00:44 -0300

    Configurancao inicial do PROJETO. Pois ele não vai fazer parte do SAUDE DIGITAL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,22 @@
+import { HeaderModule } from './core/header/header.module';
 import { NgModule } from '@angular/core';
 import { SesaSdBootStrapModule } from 'sesa-sd-commons';
 import { environment } from './../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+// OBSERVAÇÃO no PACKAGE está a dependencia do PRIMENG
+// è necessario pois estamos em migração para o PRIME. por tanto estamos utilizando o Material e o PRIME
 @NgModule({
 	declarations: [AppComponent],
 	imports: [
+		HeaderModule,
 		SesaSdBootStrapModule.forRoot({
 			URL_SERVIDOR: environment.apiUrl,
 			USUARIO: environment.USUARIO,
 			DESENVOLVIMENTO: environment.DESENVOLVIMENTO
 				? environment.DESENVOLVIMENTO
 				: false,
-			URL_AUTH: environment.urlAuth,
-			// Esse seriam os MENUS do Saude DIGITAL!
-			// Mas o sistema de vocês não fazem parte do Digital então tem essa opção aqui para criar menu proprio.
-			// Na nossa LIB hoje está sendo necessária passar o sistema como parametro. Caso contrário será listado o menu do Saude Digital
-			// A medida do tempo podemos conversar sobre... caso vocês não queiram esse layout... bastaria retirar o selector sesa-sd-bootstrap do app.componenet.HTML
-			SISTEMAS: [
-				{
-					descricao: 'HASMart',
-					listaMenus: [{ descricao: 'PAGINA', rota: 'pagina' }]
-				}
-			]
+			URL_AUTH: environment.urlAuth
 		}),
 		AppRoutingModule
 	],
